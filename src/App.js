d6070345d5a1ead54ff1803bbfc58eb6c9dcd975
Tushar <sinhatushar9234@gmail.com>
2022-03-22 00:31:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,32 @@ import './App.css';
 import Navbar from './Component/Navbar';
 import TextForm from './Component/TextForm';
 import React, {useState} from 'react'
+import Alert from './Component/Alert';
 
 
 
 function App() {
   
     const[mode,setMode]=useState("light")
+ 
+    const[alert,setAlert]=useState("")
+    const showAlert=(message,type)=>{
+      setAlert({
+        msg:message,
+        type:type
+      })
+    }
 
     const toogleMode=()=>{
       if(mode==='light'){
         setMode("dark");
         document.body.style.backgroundColor="#212529";
+        showAlert("Dark Mode has been enabled","Success");
       }
       else{
           setMode("light");
           document.body.style.backgroundColor="white";
+          showAlert("Light Mode has been enabled","Success");
       }
     }
 
@@ -28,7 +39,8 @@ function App() {
    
    <Navbar title="TextUtils" mode={mode} toogleMode={toogleMode}/>
 
-      <div className="container " >
+      <div className="container-fluid " >
+      <Alert alert={alert}/>
       <TextForm heading="Enter the text to analyze" mode={mode}/>
       {/* <About mode={mode} toogleMode={toogleMode}/> */}
       </div>
